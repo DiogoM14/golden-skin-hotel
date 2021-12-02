2b79a80af1d784ed6b5b0e0f486c84e3f52ad08f
Pedro Costa <pedrocosta2406@protonmail.com>
2021-12-02 19:49:16 +0000

    Created Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,66 @@
+import {
+  Image,
+  Box,
+  Heading,
+  Text,
+  Button,
+  SimpleGrid,
+  Icon,
+  Center,
+} from "@chakra-ui/react";
+import { FiTv, FiWifi } from "react-icons/fi";
+
+export const Card = () => {
+  const property = {
+    imageUrl:
+      "https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+  };
+
+  return (
+    <Box maxW='xs' borderRadius='lg' overflow='hidden' position='relative'>
+      <SimpleGrid
+        columns={2}
+        spacing='2'
+        position='absolute'
+        top='3'
+        right='3'
+        zIndex='100'>
+        <Center bg='white' p='2' borderRadius='lg' boxSize='30px'>
+          <Icon as={FiTv} boxSize='1.25rem' color='green' />
+        </Center>
+        <Center bg='white' p='2.5' borderRadius='lg' boxSize='30px'>
+          <Icon as={FiWifi} boxSize='1.25rem' color='blue' />
+        </Center>
+      </SimpleGrid>
+      <Image maxH='200px' width='100%' src={property.imageUrl} />
+      <Box p='6' bgColor='white'>
+        <Heading as='h6' size='md' fontWeight='Medium' mb='2'>
+          Single room
+        </Heading>
+        <Text color='gray.500' fontSize='sm' mb='6'>
+          Lorem ipsum dolor sit amet, consect adipiscing elit.
+        </Text>
+        <Box display='flex' mb='6'>
+          <Text fontSize='sm' mr='1' color='gray.500'>
+            Desde
+          </Text>
+          <Text fontSize='sm' fontWeight='bold' color='gray.900'>
+            47€ / noite
+          </Text>
+        </Box>
+        <Button
+          fontFamily='Poppins'
+          fontWeight='Medium'
+          fontSize='sm'
+          bgColor='white'
+          border='2px'
+          borderColor='#F2BB05'
+          width='125px'
+          height='30px'
+          _hover={{ bg: "transparent" }}>
+          Ver Mais
+        </Button>
+      </Box>
+    </Box>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,26 @@
-import Head from 'next/head'
-import type { NextPage } from 'next'
+import Head from "next/head";
+import type { NextPage } from "next";
 
-import { Container } from '@chakra-ui/react'
-import { Header } from '../components/Header'
+import { Container } from "@chakra-ui/react";
+import { Header } from "../components/Header";
+import { Card } from "../components/Card";
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Golden Skin Hotel</title>
-        <meta name="description" content="Melhor hotel do mundo" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Melhor hotel do mundo' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <Header />
 
-      <Container maxW="1440px">
-        
+      <Container maxW='1440px'>
+        <Card />
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
