63bac0e00b3b432453fcdec41e3ec2b3427c99fe
Pedro Costa <pedrocosta2406@protonmail.com>
2021-12-23 17:48:58 +0000

    Heroku deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,19 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+import axios from "axios";
+import { parseCookies } from "nookies";
 
 export function setupAPIClient(ctx = undefined) {
-  let cookies = parseCookies(ctx)
+  let cookies = parseCookies(ctx);
 
   const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: "https://golden-skin-hotel.herokuapp.com",
     // headers: {
     //   "x-access-token": `${cookies['nextauth.token']}`
     // }
-  })
-  
-  api.interceptors.response.use(response => {
-    return response
-  })
+  });
 
-    return api
+  api.interceptors.response.use((response) => {
+    return response;
+  });
+
+  return api;
 }
