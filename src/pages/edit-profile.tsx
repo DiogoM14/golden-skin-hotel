31daaf198ba3323c51fb6668cbe033a5ee6d3a5f
Pedro Costa <pedrocosta2406@protonmail.com>
2021-12-26 18:24:09 +0000

    Improved user profile edit page

--- src/pages/edit-profile.tsx
+++ src/pages/edit-profile.tsx
@@ -18,6 +18,7 @@ import { AuthContext } from "../contexts/AuthContext";
 import Router from "next/router";
 import { api } from "../services/apiClient";
 import { parseCookies } from "nookies";
+import { useForm } from "react-hook-form";
 
 type userInfo = {
   email: string;
@@ -39,6 +40,12 @@ type userInfo = {
 export const EditProfile: NextPage = () => {
   const { isAuthenticated, user } = useContext(AuthContext);
   const [userInfo, setUserInfo] = useState<userInfo>();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+    setValue,
+  } = useForm();
 
   useEffect(() => {
     if (!isAuthenticated) {
@@ -52,11 +59,54 @@ export const EditProfile: NextPage = () => {
             "x-access-token": token,
           },
         })
-        .then((response) => {
-          setUserInfo(response.data);
+        .then((res) => {
+          res.data.email && setValue("email", res.data.email);
+          res.data.first_name && setValue("firstName", res.data.first_name);
+          res.data.last_name && setValue("lastName", res.data.last_name);
+          res.data.phone_number &&
+            setValue("phoneNumber", res.data.phone_number);
+          res.data.birthday && setValue("birthday", res.data.birthday);
+          res.data.address &&
+            (setValue("street", res.data.address.street),
+            setValue("city", res.data.address.city),
+            setValue("postalCode", res.data.address.postal_code),
+            setValue("country", res.data.address.country));
+          res.data.nif && setValue("nif", res.data.nif);
+
+          setUserInfo(res.data);
         });
     }
   }, []);
+
+  const onSubmit = (values: any) => {
+    const { "nextauth.token": token } = parseCookies();
+
+    Object.keys(values).map((key) => {
+      if (values[key] === "") {
+        delete values[key];
+      }
+    });
+
+    if (values.street || values.city || values.postal_code || values.country) {
+      values.address = {
+        street: values.street ? values.street : null,
+        city: values.city ? values.city : null,
+        postal_code: values.postal_code ? values.postal_code : null,
+        country: values.country ? values.country : null,
+      };
+    }
+
+    api
+      .put("/me/myInfo", values, {
+        headers: {
+          "x-access-token": token,
+        },
+      })
+      .then((res) => {
+        setUserInfo(res.data);
+      });
+  };
+
   return (
     <>
       <Head>
@@ -85,90 +135,123 @@ export const EditProfile: NextPage = () => {
                 Alterar foto de perfil
               </Text>
             </Flex>
-            <FormControl mt='3'>
-              <SimpleGrid columns={2} rows={5} spacingX={"20px"}>
-                <GridItem colSpan={2}>
-                  <FormLabel fontWeight='regular' htmlFor='email'>
-                    E-mail
-                  </FormLabel>
-                  <Input
-                    value={userInfo && userInfo.email}
-                    type='email'
-                    id='email'
-                  />
-                </GridItem>
-                <GridItem colSpan={1}>
-                  <FormLabel fontWeight='regular' htmlFor='fistName'>
-                    Primeiro nome
-                  </FormLabel>
-                  <Input
-                    value={userInfo && userInfo.first_name}
-                    type='text'
-                    id='firstName'
-                  />
-                </GridItem>
-                <GridItem colSpan={1}>
-                  <FormLabel fontWeight='regular' htmlFor='lastName'>
-                    Último nome
-                  </FormLabel>
-                  <Input
-                    value={userInfo && userInfo.last_name}
-                    type='text'
-                    id='lastName'
-                  />
-                </GridItem>
-                <GridItem colSpan={2}>
-                  <FormLabel fontWeight='regular' htmlFor='telephone'>
-                    Telemóvel
-                  </FormLabel>
-                  <Input
-                    value={userInfo && userInfo.phone_number}
-                    type='tel'
-                    id='telephone'
-                  />
-                </GridItem>
-                <GridItem colSpan={2}>
-                  <FormLabel fontWeight='regular' htmlFor='country'>
-                    País
-                  </FormLabel>
-                  <Input
-                    value={userInfo && userInfo.address.country}
-                    type='text'
-                    id='country'
-                  />
-                </GridItem>
-                <GridItem colSpan={1}>
-                  <FormLabel fontWeight='regular' htmlFor='address'>
-                    Morada
-                  </FormLabel>
-                  <Input
-                    value={userInfo && userInfo.address.street}
-                    type='text'
-                    id='address'
-                  />
-                </GridItem>
-                <GridItem colSpan={1}>
-                  <FormLabel fontWeight='regular' htmlFor='postalCode'>
-                    Código Postal
-                  </FormLabel>
-                  <Input
-                    value={userInfo && userInfo.address.postal_code}
-                    type='text'
-                    id='postalCode'
-                  />
-                </GridItem>
-              </SimpleGrid>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <FormControl mt='3'>
+                <SimpleGrid columns={2} rows={5} spacingX={"20px"}>
+                  <GridItem colSpan={1}>
+                    <FormLabel fontWeight='regular' htmlFor='first_name'>
+                      Primeiro nome
+                    </FormLabel>
+                    <Input
+                      defaultValue={userInfo?.first_name && userInfo.first_name}
+                      type='text'
+                      id='first_name'
+                      {...register("first_name")}
+                    />
+                  </GridItem>
+                  <GridItem colSpan={1}>
+                    <FormLabel fontWeight='regular' htmlFor='last_name'>
+                      Último nome
+                    </FormLabel>
+                    <Input
+                      defaultValue={userInfo?.last_name && userInfo.last_name}
+                      type='text'
+                      id='last_name'
+                      {...register("last_name")}
+                    />
+                  </GridItem>
+                  <GridItem colSpan={2}>
+                    <FormLabel fontWeight='regular' htmlFor='telephone'>
+                      Telemóvel
+                    </FormLabel>
+                    <Input
+                      defaultValue={
+                        userInfo?.phone_number && userInfo.phone_number
+                      }
+                      type='tel'
+                      id='telephone'
+                      {...register("telephone")}
+                    />
+                  </GridItem>
+                  <GridItem colSpan={2}>
+                    <FormLabel fontWeight='regular' htmlFor='nif'>
+                      NIF
+                    </FormLabel>
+                    <Input
+                      defaultValue={userInfo?.nif && userInfo.nif}
+                      type='text'
+                      id='nif'
+                      {...register("nif")}
+                    />
+                  </GridItem>
+                  <GridItem colSpan={2}>
+                    <FormLabel fontWeight='regular' htmlFor='country'>
+                      País
+                    </FormLabel>
+                    <Input
+                      defaultValue={
+                        userInfo?.address?.country && userInfo.address.country
+                      }
+                      type='text'
+                      id='country'
+                      {...register("country")}
+                    />
+                  </GridItem>
+                  <GridItem colSpan={1}>
+                    <FormLabel fontWeight='regular' htmlFor='street'>
+                      Rua
+                    </FormLabel>
+                    <Input
+                      defaultValue={
+                        userInfo?.address?.street && userInfo.address.street
+                      }
+                      type='text'
+                      id='street'
+                      {...register("street")}
+                    />
+                  </GridItem>
+                  <GridItem colSpan={1}>
+                    <FormLabel fontWeight='regular' htmlFor='postal_code'>
+                      Código Postal
+                    </FormLabel>
+                    <Input
+                      defaultValue={
+                        userInfo?.address?.postal_code &&
+                        userInfo.address.postal_code
+                      }
+                      type='text'
+                      id='postal_code'
+                      {...register("postal_code")}
+                    />
+                  </GridItem>
+                  <GridItem colSpan={2}>
+                    <FormLabel fontWeight='regular' htmlFor='city'>
+                      Localidade
+                    </FormLabel>
+                    <Input
+                      defaultValue={
+                        userInfo?.address?.city && userInfo.address.city
+                      }
+                      type='text'
+                      id='city'
+                      {...register("city")}
+                    />
+                  </GridItem>
+                </SimpleGrid>
 
-              <Center mt='6' mb='20'>
-                <Button
-                  color='#fff'
-                  bgColor='#F2BB05'
-                  _hover={{ bg: "#e0ae09" }}
-                  size='lg'>
-                  Guardar
-                </Button>
-              </Center>
-            </FormControl>
+                <Center mt='6' mb='20'>
+                  <Button
+                    color='#fff'
+                    bgColor='#F2BB05'
+                    _hover={{ bg: "#e0ae09" }}
+                    size='lg'
+                    type='submit'
+                    isLoading={isSubmitting}>
+                    Guardar
+                  </Button>
+                </Center>
+              </FormControl>
+            </form>
           </Flex>
         </Center>
       </Container>
