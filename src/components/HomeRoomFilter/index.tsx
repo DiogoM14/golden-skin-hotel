a14abdeda96c01271580cc88508f47fe0179a0d8
Pedro Costa <pedrocosta2406@protonmail.com>
2021-12-26 16:02:51 +0000

    Improved HomeRoomFilter check in/out date picking

--- src/components/HomeRoomFilter/index.tsx
+++ src/components/HomeRoomFilter/index.tsx
@@ -11,51 +11,44 @@ import {
   Center,
   Icon,
   Text,
-  Box,
+  useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { useRouter } from "next/router";
-import DatePicker, { registerLocale } from "react-datepicker";
-import { FiCalendar, FiUsers } from "react-icons/fi";
-import { FilterContent } from "./FilterContent";
-import format from "date-fns/format";
-import pt from "date-fns/locale/pt";
-registerLocale("pt", pt);
-import "react-datepicker/dist/react-datepicker.css";
+import { FiUsers } from "react-icons/fi";
+import { HomeResDates } from "./HomeResDates";
 
 export const HomeRoomFilter = () => {
   const router = useRouter();
-  const [isOpen, setIsOpen] = useState(false);
-  const [startDate, setStartDate] = useState(new Date());
+  const toast = useToast();
+  const [startDate, setStartDate] = useState(new Date() as any);
   const [endDate, setEndDate] = useState(null);
   const [people, setPeople] = useState(1);
-
-  const onChange = (dates: [any, any]) => {
-    const [start, end] = dates;
-    setStartDate(start);
-    setEndDate(end);
-    setIsOpen(false);
-  };
-
-  const handleOpen = () => {
-    setIsOpen(!isOpen);
-    return true;
-  };
 
   const handlePeople = (e: any) => {
     setPeople(e);
   };
 
   const handleSearch = () => {
-    endDate &&
-      router.push({
-        pathname: "/rooms",
-        query: {
-          checkIn: format(startDate, "yyyy-MM-dd"),
-          checkOut: format(endDate, "yyyy-MM-dd"),
-          capacityGT: people,
-        },
-      });
+    if (endDate) {
+      if (startDate > endDate) {
+        toast({
+          title: "Check-in date must be before check-out date",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        router.push({
+          pathname: "/rooms",
+          query: {
+            checkIn: startDate,
+            checkOut: endDate,
+            capacityGT: people,
+          },
+        });
+      }
+    }
   };
 
   return (
@@ -72,39 +65,20 @@ export const HomeRoomFilter = () => {
         px='4'
         zIndex='2'
         position='relative'>
-        <Button
-          onClick={handleOpen}
-          bgColor='transparent'
-          _hover={{ bgColor: "transparent" }}>
-          <FilterContent
-            icon={FiCalendar}
-            name='Check-in'
-            description={format(startDate, "dd-MMM-yyyy", {
-              locale: pt,
-            })}
-          />
-        </Button>
-
-        <Divider orientation='vertical' mx={{ base: 5, lg: 14 }} />
-
-        <Button
-          onClick={handleOpen}
-          bgColor='transparent'
-          _hover={{ bgColor: "transparent" }}>
-          <FilterContent
-            icon={FiCalendar}
-            name='Check-out'
-            description={
-              endDate
-                ? format(endDate, "dd-MMM-yyyy", {
-                    locale: pt,
-                  })
-                : "Sem data"
+        <HomeResDates
+          checkIn={startDate}
+          checkOut={endDate}
+          handleDateChanges={(date: any) => {
+            if (date.isCheckIn) {
+              setStartDate(date.date);
             }
-          />
-        </Button>
+            if (!date.isCheckIn) {
+              setEndDate(date.date);
+            }
+          }}
+        />
 
-        <Divider orientation='vertical' mx={{ base: 5, lg: 14 }} />
+        <Divider orientation='vertical' mx={{ base: 8, lg: 14 }} />
 
         <Center>
           <Flex>
@@ -130,7 +104,7 @@ export const HomeRoomFilter = () => {
           </Flex>
         </Center>
 
-        <Divider orientation='vertical' mx={{ base: 5, lg: 14 }} />
+        <Divider orientation='vertical' mx={{ base: 8, lg: 14 }} />
 
         <Button
           bgColor='#F2BB05'
@@ -140,26 +114,6 @@ export const HomeRoomFilter = () => {
           onClick={handleSearch}>
           Procurar
         </Button>
-
-        {isOpen && (
-          <Box
-            position='absolute'
-            top='100px'
-            zIndex='1000'
-            boxShadow='dark-lg'>
-            <DatePicker
-              closeOnScroll={handleOpen}
-              selected={startDate}
-              onChange={onChange}
-              startDate={startDate}
-              endDate={endDate}
-              minDate={new Date()}
-              selectsRange
-              inline
-              locale={pt}
-            />
-          </Box>
-        )}
       </Flex>
     </VStack>
   );
