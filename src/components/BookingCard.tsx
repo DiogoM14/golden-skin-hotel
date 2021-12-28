add588616a39fc4c71384630a104baf0eeb744e9
Pedro Costa <pedrocosta2406@protonmail.com>
2021-12-28 20:08:36 +0000

    User bookings page

---
+++ src/components/BookingCard.tsx
@@ -0,0 +1,129 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  SimpleGrid,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import { format } from "date-fns";
+import pt from "date-fns/locale/pt";
+import NextLink from "next/link";
+
+export const BookingCard = ({ booking }: any) => {
+  return (
+    <>
+      <Flex
+        direction={{ base: "column", lg: "row" }}
+        borderRadius='lg'
+        boxShadow='md'
+        borderWidth='1px'
+        p='4'
+        mb='8'>
+        <Flex flex='1' direction='column' mr='4'>
+          <Heading fontWeight='bold' fontSize='lg'>
+            Detalhes da reserva
+          </Heading>
+          <Divider my='4' borderColor='#bbbbbb' />
+          <SimpleGrid columns={{ base: 1, sm: 1, md: 3 }} height='100%'>
+            <Flex direction='column' justify='space-evenly'>
+              <HStack>
+                <Text fontSize='md' fontWeight='medium'>
+                  Nº de hóspedes:
+                </Text>
+                <Text>{booking.no_guests}</Text>
+              </HStack>
+
+              <HStack>
+                <Text fontSize='md' fontWeight='medium'>
+                  Preço total:
+                </Text>
+                <Text>{booking.final_price}€</Text>
+              </HStack>
+              <HStack>
+                <Text fontSize='md' fontWeight='medium'>
+                  Datas:
+                </Text>
+                <Text>
+                  {format(new Date(booking.dates.from), "dd MMM yyyy", {
+                    locale: pt,
+                  }) +
+                    " - " +
+                    format(new Date(booking.dates.to), "dd MMM yyyy", {
+                      locale: pt,
+                    })}
+                </Text>
+              </HStack>
+              <HStack>
+                <Text fontSize='md' fontWeight='medium'>
+                  Nº de noite:
+                </Text>
+                <Text>{booking.no_nights}</Text>
+              </HStack>
+            </Flex>
+
+            <Flex>
+              <Text fontSize='md' fontWeight='medium' mr='2'>
+                Extras:
+              </Text>
+              {booking.extras.length >= 1 ? (
+                <List>
+                  {booking.extras.map((extra: any) => (
+                    <ListItem key={extra}>{extra}</ListItem>
+                  ))}
+                </List>
+              ) : (
+                <>
+                  <Text>Sem Extras</Text>
+                </>
+              )}
+            </Flex>
+            <Box overflow='hidden'>
+              <Text fontSize='md' fontWeight='medium'>
+                Observações:
+              </Text>
+              <Text noOfLines={4}>
+                {booking.observations ? booking.observations : "Nenhuma"}
+              </Text>
+            </Box>
+          </SimpleGrid>
+        </Flex>
+        <Box
+          flex='1/2'
+          overflow='hidden'
+          position='relative'
+          h='100%'
+          mt={{ base: 4, lg: 0 }}>
+          <Image
+            borderRadius='lg'
+            maxH={{ sm: "150px", lg: "200px" }}
+            h='100%'
+            width='100%'
+            src={booking.room.images[0]}
+            objectFit='cover'
+          />
+          <NextLink href={`/room/${booking.room._id}`}>
+            <Button
+              color='#fff'
+              bgColor='#F2BB05'
+              _hover={{ bg: "#e0ae09" }}
+              position='absolute'
+              size='sm'
+              bottom='0'
+              right='0'
+              mr='4'
+              mb='4'>
+              Ver quarto
+            </Button>
+          </NextLink>
+        </Box>
+      </Flex>
+    </>
+  );
+};
--- src/pages/my-bookings.tsx
+++ src/pages/my-bookings.tsx
@@ -1,12 +1,15 @@
-import { Container } from "@chakra-ui/react";
+import { Center, Container, Flex, Heading, Text } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Head from "next/head";
+import NextLink from "next/link";
 import { parseCookies } from "nookies";
 import { useEffect, useState } from "react";
+import { BookingCard } from "../components/BookingCard";
+
 import { api } from "../services/apiClient";
 
 export const MyBookings: NextPage = () => {
-  const [bookings, setBookings] = useState([]);
+  const [bookings, setBookings] = useState([] as any[]);
 
   useEffect(() => {
     const { "nextauth.token": token } = parseCookies();
@@ -17,7 +20,7 @@ export const MyBookings: NextPage = () => {
         },
       })
       .then((res) => {
-        setBookings(res.data);
+        setBookings(res.data.bookings);
       });
   }, []);
 
@@ -28,7 +31,43 @@ export const MyBookings: NextPage = () => {
         <meta name='description' content='Melhor hotel do mundo' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Container></Container>
+      <Container maxW='container.xl' px={["6", "8", "12"]} mt='2.3rem' w='100%'>
+        <Heading
+          fontFamily='ubuntu'
+          fontSize='4xl'
+          fontWeight='bold'
+          mt='8'
+          mb='12'>
+          Minhas reservas
+        </Heading>
+        {bookings.length >= 1 ? (
+          <>
+            <Flex direction='column'>
+              {bookings.map((booking) => (
+                <BookingCard key={booking._id} booking={booking} />
+              ))}
+            </Flex>
+          </>
+        ) : (
+          <Center mb='16'>
+            <Flex direction='column'>
+              <Heading fontSize='2xl' fontWeight='regular' mb='4'>
+                Ainda não fizeste reservas
+              </Heading>
+              <Center>
+                <Flex>
+                  <Text mr='1'>Começa por</Text>
+                  <NextLink href='/rooms'>
+                    <Text cursor='pointer' fontWeight='semibold'>
+                      procurar um quarto aqui
+                    </Text>
+                  </NextLink>
+                </Flex>
+              </Center>
+            </Flex>
+          </Center>
+        )}
+      </Container>
     </>
   );
 };
